26a427874d82b626a249e04b8f16c66b87c63759
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-03-01 09:15:59 +0530

    part-2-ready

--- src/component/Testimonial.js
+++ src/component/Testimonial.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Footer from "./Footer"
 import Header from "./Header"
+import "../CSS/testimonial.css"
+import {testimonials} from "./testimonials"
 export default function Testimonial() {
   return (
     <>
     <Header/>
-    <div>Testimonial</div>
+    <MainContent/>
     <Footer/>
     </>
   )
+}
+
+
+function MainContent(){
+    return(
+      <div className='main-testimonial-body'>
+          {Object.values(testimonials).map((data)=>{
+            return(
+              <div className='testimonial-content-body'>
+                <div className="testimonial-img">
+                  <img src={data.img} alt=""/>
+                </div>
+                <div className='testimonial-content'>
+                  <div>
+                      {data.msg}
+                  </div>
+                  <div className='testimonial-name'>
+                        {data.name}
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+      </div>
+    )
 }
---
+++ src/component/testimonials.js
@@ -0,0 +1,22 @@
+let Server_Url="/startup_saturday"
+const testimonials={
+    1:{"msg":`Overjoyed & delighted to announce that Amit Chaudhary,
+    Co-Founder & CTO,Lenskart believes in Briefly and it’s mission in
+    building the new internet for the field of 360 degree Marketing,
+    Technology & Production and is coming as the FirstAngel &
+    Mentor.The level of support and love which I have received from
+    him is truly haeart warming,and I will aim to do justice to his
+    faith in us.`,"img":`${Server_Url}/testimonial1.jpg`,"name":"Alankrit Utkrash"},
+    2:{"msg":`It was a pleasure to meet the Business tycoon of India Amit
+    Chaudhary Co-Founder of Lenskart.Had an amazing interactive
+    session which was apart of Startup Saturday initiative.Also,got
+    a chance to discuss the expansion plans of the brand.`,"img":`${Server_Url}/testimonial1.jpg`,"name":"Paras Satija"},
+    3:{"msg":`Had a fruitful strategic & road map discussion for HOPPUP with
+    no other then Amit Chaudhary.It was a pleasure to e-meet you.
+    All thanks to Lenskart.com for initiating Startup Saturdays and
+    giving a platform to budding entrepeneurs at early stage to be
+    mentored.Thanks to Rima Kapoor for giving us an opportunity
+    to be there.`,"img":`${Server_Url}/testimonial1.jpg`,"name":"Deepak Arora"},
+}
+
+export {testimonials}
